6e86d91a628f0f7c983388da788c5b5e55d13687
arjitchauhan <arjitchauhan@localhost>
2022-09-06 16:42:47 +0530

    New Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,27 +5,29 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the no. of elemments : ");
-		int n = sc.nextInt();
-		int arr[]= new int[n];
-		int temp;
-		System.out.println("Enter the elements :-");
-		for(int i =0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for(int j =0; j < n; j++) {
-		for(int i =0; i < n-1; i++) {
-			if(arr[i] > arr[i+1]) {
-				temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
+		try (
+		Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the no. of elemments : ");
+			int n = sc.nextInt();
+			int arr[]= new int[n];
+			int temp;
+			System.out.println("Enter the elements :-");
+			for(int i =0; i < n; i++) {
+				arr[i] = sc.nextInt();
 			}
-		}
-		}
-		System.out.println("Sorted array is :- ");
-		for(int i =0; i < n; i++) {
-			System.out.println(arr[i] + " ");
+			for(int j =0; j < n; j++) {
+			for(int i =0; i < n-1; i++) {
+				if(arr[i] > arr[i+1]) {
+					temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				}
+			}
+			}
+			System.out.println("Sorted array is :- ");
+			for(int i =0; i < n; i++) {
+				System.out.println(arr[i] + " ");
+			}
 		} 
 	}
 
